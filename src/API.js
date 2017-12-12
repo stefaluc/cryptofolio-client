7c1fe1ffbaf7813dbcf7937975ab63ccb5d94c39
Lucas Stefanski <stefaluc000@gmail.com>
2017-12-12 22:03:31 +0700

    Finish API link

--- src/API.js
+++ src/API.js
@@ -7,8 +7,9 @@ class API {
   static login(email, password) {
     const payload = {
       "username": email,
-      "password": password
+      password,
     }
+
     return new Promise((resolve, reject) => {
       axios.post(baseURL + 'auth', payload)
         .then((response) => {
@@ -26,17 +27,19 @@ class API {
         .catch(function (error) {
           reject(error)
         });
-    })
+    });
   }
-  static signup(email, password, firstName, lastName, currency, gRecaptchaResponse) {
+
+  static signup(email, password, firstName, lastName, favouriteCurrencyID, gRecaptchaResponse) {
     const payload = {
       "username": email,
-      "password": password,
-      "firstName": firstName,
-      "lastName": lastName,
-      "favouriteCurrencyId": currency,
-      "gRecaptchaResponse": gRecaptchaResponse,
+      password,
+      firstName,
+      lastName,
+      favouriteCurrencyID,
+      gRecaptchaResponse,
     }
+
     return new Promise((resolve, reject) => {
       axios.post(baseURL + 'signup', payload)
         .then((response) => {
@@ -50,10 +53,89 @@ class API {
             reject("Username does not exists")
           }
         })
-        .catch(function (error) {
+        .catch((error) => {
           reject(error)
         });
-    })
+    });
+  }
+
+  static getBalances() {
+    const token = localStorage.getItem('cryptfolioToken');
+
+    return new Promise((resolve, reject) => {
+      axios.get(baseURL + 'auth/balances?token=' + token)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  static getTransactions() {
+    const token = localStorage.getItem('cryptfolioToken');
+
+    return new Promise((resolve, reject) => {
+      axios.get(baseURL + 'auth/transactions?token=' + token)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  static getCurrencies() {
+    const token = localStorage.getItem('cryptfolioToken');
+
+    return new Promise((resolve, reject) => {
+      axios.get(baseURL + 'auth/currencies?token=' + token)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  static insertBalance(cryptocurrencyID, quantity) {
+    const payload = {
+      cryptocurrencyID,
+      quantity,
+    }
+    const token = localStorage.getItem('cryptfolioToken');
+
+    return new Promise((resolve, reject) => {
+      axios.post(baseURL + 'auth/balance?token=' + token, payload)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  static insertTransaction(balanceID, quantity, price, date) {
+    const payload = {
+      quantity,
+      price,
+      date,
+    }
+    const token = localStorage.getItem('cryptfolioToken');
+
+    return new Promise((resolve, reject) => {
+      axios.post(baseURL + 'auth/balance/' + balanceID + '?token=' + token, payload)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 }
 
